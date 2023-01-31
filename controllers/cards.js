33ce79b8d128ad5c88496ccda0d21cbd6401027f
Aleksandr Viznyi <senak98@yandex.ru>
2023-02-01 01:51:31 +0400

    add new controllers and routes for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,4 +22,26 @@ const deleteCard = (req, res) => {
     .catch((err) => res.status(500).send(err));
 };
 
-module.exports = { getCards, createCard, deleteCard };
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => { res.status(200).send({ data: card }); })
+    .catch((err) => res.status(500).send(err));
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => { res.status(200).send({ data: card }); })
+    .catch((err) => res.status(500).send(err));
+};
+
+module.exports = {
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
+};
