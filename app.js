729b4fbb406d8ae3a37a6a5e6303e4a2a14ba29e
Aleksandr Viznyi <senak98@yandex.ru>
2023-02-01 21:54:25 +0400

    id

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-    _id: '63d957ebf3bc5525f6ca12cc',
+    _id: '63d957ebf3bc5525f6ca12cc'
   };
 
   next();
